62a3bbc83356ecafd13b26d73ee61c97d1823a2e
mamoluke <118966946+mamoluke@users.noreply.github.com>
2025-01-25 17:04:13 +0900

    Delete js directory

--- js/script.js
+++
@@ -1,8 +0,0 @@
-// Smooth scrolling for navigation links
-document.querySelectorAll('nav ul li a').forEach(link => {
-  link.addEventListener('click', (e) => {
-    e.preventDefault();
-    const targetId = link.getAttribute('href').substring(1);
-    document.getElementById(targetId).scrollIntoView({ behavior: 'smooth' });
-  });
-});
